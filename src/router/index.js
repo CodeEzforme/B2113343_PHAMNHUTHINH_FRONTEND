8b06eb113ecfc77d28b07ad2eae7e350fad85e4d
CodeEzforme <144590228+CodeEzforme@users.noreply.github.com>
2024-03-19 15:38:45 +0700

    Tạo trang cập nhật Liên hệ

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,24 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 import NotFound from "@/views/NotFound.vue";
+import ContactEdit from "@/views/ContactEdit.vue";
 
 const routes = [
     {
         path: "/",
         name: "contactbook",
         component: ContactBook,
-    },{
+    },
+    {
         path: "/:pathMatch(.*)*",
         name: "NotFound",
         component: NotFound,
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: ContactEdit,
+        props: true
     },
 ];
 
